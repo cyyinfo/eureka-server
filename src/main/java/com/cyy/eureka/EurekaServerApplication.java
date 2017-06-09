fa6f64c2f95fa71aaac80d6e0ebf2e04ca521d52
chenyuanyuan <719591157@qq.com>
2017-06-09 14:58:00 +0800

    spring cloud eureka 注册中心

---
+++ src/main/java/com/cyy/eureka/EurekaServerApplication.java
@@ -0,0 +1,10 @@
+package com.cyy.eureka;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+}
