bcaff24aa64445215d4dee425f4a7342abea3672
陈园园 <719591157@qq.com>
2017-06-10 13:57:25 +0800

    spring-boot 版本升级到 1.5.2

--- src/main/java/com/cyy/eureka/EurekaServerApplication.java
+++ src/main/java/com/cyy/eureka/EurekaServerApplication.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+
+/**
+ * 注册中心  其他服务需要注册到该中心，统一调度
+ * @author cyy
+ */
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
